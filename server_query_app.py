676ca768f7d39f880ce42e6c9fb20841b6e7a760
bandeep20001 <bandeep2000@gmail.com>
2024-07-18 11:53:49 -0700

    modified with sample queries

--- server_query_app.py
+++ server_query_app.py
@@ -21,25 +21,43 @@ agent = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True,age
 st.title("CMDB Query Assistant")
 
 # TODO spinning wheel, modify create_engine, create dockerfile,sample inputs
+# readme should have details of db with foreigh key
 
-query = st.text_input("Enter your question about the CMDB:")
+# Sample queries
+sample_queries = [
+    "How many Windows servers do we have?",
+    "List all Ubuntu servers with their RAM and disk space.",
+    "What applications are installed on the server with IP 192.168.1.10?",
+    "Show me all servers in the Production environment.",
+    "Which server has the most CPU cores?"
+]
+
+# Text input with sample query
+query = st.text_input(
+    "Enter your question about the CMDB:",
+    placeholder=f"e.g., {sample_queries[1]}"
+)
+
+#query = st.text_input("Enter your question about the CMDB:")
+
 
 if query:
     try:
-        # Execute the agent to get SQL and result
-        response = agent.invoke(query)
-        
-        # get sql query details 
-        sql_query = []
-        for (log, output) in response["intermediate_steps"]:
-            if log.tool == 'sql_db_query':
-                sql_query.append(log.tool_input)
-        st.code(sql_query, language="sql")
+         with st.spinner("Executing query..."):
+            # Execute the agent to get SQL and result
+            response = agent.invoke(query)
+            
+            # get sql query details 
+            sql_query = []
+            for (log, output) in response["intermediate_steps"]:
+                if log.tool == 'sql_db_query':
+                    sql_query.append(log.tool_input)
+            st.code(sql_query, language="sql")
 
-        # write the LLM response
-        st.header("LLM Response")
-        st.write(response["output"])
-        #print(response)
+            # write the LLM response
+            st.header("LLM Response")
+            st.write(response["output"])
+            #print(response)
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
